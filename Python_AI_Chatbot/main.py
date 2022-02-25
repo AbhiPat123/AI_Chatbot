2e1e5b9d537321e35c46d935f92c9ddc06660749
AbhiPat123 <abhipat1996@gmail.com>
2022-02-25 16:29:54 +0300

    Add speech to text basic function to chatbot

--- Python_AI_Chatbot/main.py
+++ Python_AI_Chatbot/main.py
@@ -3,6 +3,8 @@
 # PCKAGES:
     # - Numpy               - basic functionality on numbers
     # - SpeechRecognition   - for text to Speech
+    #    - pyAudio          - to work with recognizer
+    #        - pipwin       - then install pyAudio using 'pipwin install pyaudio'
     # - gTTS                - speech to text and speak it out
     # - transformers        - to build lanugage models
     # - tensorflow          - ""
@@ -24,15 +26,25 @@ class ChatBot():
     def speech_to_text(self):
         recognizer = sr.Recognizer()
         with sr.Microphone() as mic:
-             print("listening...")
+             print("Listening to you speak...")
              audio = recognizer.listen(mic)
         try:
-             self.text = recognizer.recognize_google(audio)
-             print("me --> ", self.text)
-        except:
-             print("me -->  ERROR")
+            # Sphinx, Google, Google Cloud, Wit.ai, Bing, Azure, Houndify, IBM - each has their own recognize_...(audio) function
+            # NOTE:- each of them has different number of parameters like credentials to use their service
+            self.text = recognizer.recognize_google(audio)      # uses Google
+            print("Your Bot " + self.name + " --> ", self.text)
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio!")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 # Start execution
 if __name__ == "__main__":
+    # create a Dev bot
     dev_bot = ChatBot(name="Dev")
     
+    # make the bot listen and convert to text
+    while True:
+        dev_bot.speech_to_text()
+
+    # NEXT CREATE wake_up function
